8fdfc7e33cd5eaef02a45a7277a147a69403eba1
Brandon Bankert <bankertb@members1st.org>
2021-11-01 08:58:01 -0400

    Initial commit of skeleton web api for unit testing

---
+++ UnitTestWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using UnitTestWebApi.Models;
+
+namespace UnitTestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //TODO inject DB into controller
+
+        //TODO: Add DI into constructor
+        public ProductsController()
+        {
+
+        }
+        //All basic CRUD (create,read,update,delete) operations
+        //TODO: Implement endpoints
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
