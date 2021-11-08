76a8bd1d9a7e63e80c8904bd575f81d0decae3bb
Brandon Bankert <bankertb@members1st.org>
2021-11-08 15:14:07 -0500

    Actually use the repository pattern...

--- UnitTestWebApi/Controllers/ProductsController.cs
+++ UnitTestWebApi/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnitTestWebApi.Data;
 using UnitTestWebApi.Models;
+using UnitTestWebApi.Repositories;
 
 namespace UnitTestWebApi.Controllers
 {
@@ -9,18 +13,22 @@ namespace UnitTestWebApi.Controllers
     public class ProductsController : ControllerBase
     {
         //TODO inject DB into controller
+        private IProductRepository _repository;
+
 
         //TODO: Add DI into constructor
-        public ProductsController()
+        public ProductsController(IProductRepository productRepository)
         {
-
+            _repository = productRepository;
         }
+
         //All basic CRUD (create,read,update,delete) operations
         //TODO: Implement endpoints
         [HttpGet]
-        public IActionResult GetAllProducts()
+        public ActionResult<List<Product>> GetAllProducts()
         {
-            throw new NotImplementedException();
+            //returns 200 Status code with body being the result
+            return _repository.getAllProducts();
         }
 
 
@@ -41,7 +49,7 @@ namespace UnitTestWebApi.Controllers
         [HttpPost]
         public IActionResult CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            return new OkObjectResult(product);
         }
 
         [HttpDelete]
--- WebApi.Tests/ProductsControllerTests.cs
+++ WebApi.Tests/ProductsControllerTests.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestWebApi.Controllers;
+using Moq;
+using UnitTestWebApi.Data;
+using System.Linq;
+using UnitTestWebApi.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using UnitTestWebApi.Repositories;
 
 namespace WebApi.Tests
 {
     [TestClass]
-    class ProductsControllerTests
+    public class ProductsControllerTests
     {
+        private ProductsController _controller;
+        private Mock<IProductRepository> _repositoryMock;
 
+        //run before each test
+        [TestInitialize]
+        public void Setup()
+        {
+            _repositoryMock = new Mock<IProductRepository>();
+            _controller = new ProductsController(_repositoryMock.Object);
+        }
 
+        //run after each test
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _repositoryMock = null;
+            _controller = null;
+        }
 
+        [TestMethod]
+        public void GetAllProducts_WhenTheStoreIsEmpty_ReturnsAnEmptyList()
+        {
+            //return []
+            _repositoryMock.Setup(repository => repository.getAllProducts()).Returns(new List<Product>());
+
+            var expected = new List<Product>();
+            List<Product> actual = _controller.GetAllProducts().Value;
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
     }
 }
